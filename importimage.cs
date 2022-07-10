a253fa7571c608caec926548a8eb98177c4737d6
Toast <98610769+ToastKamiya@users.noreply.github.com>
2022-07-10 21:07:21 +0300

    V1.9 Unity project files
    Does not contain SFB or TMP

---
+++ importimage.cs
@@ -0,0 +1,69 @@
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using SFB;
+using System;
+using System.IO;
+// using Unity Standalone File Browser by gkngkc on github
+// github.com/gkngkc/UnityStandaloneFileBrowser
+ 
+public class importimage : MonoBehaviour, IPointerClickHandler
+{
+ 
+    public GameObject mainImg;
+
+    public Text tutorialText;
+
+	void Start() 
+	{
+		InvokeRepeating("Process", 0, 5f);
+		StartCoroutine(FadeTextToZeroAlpha(1f, tutorialText));
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int clickCount = eventData.clickCount;
+		if (clickCount == 2)
+		{
+			var paths = StandaloneFileBrowser.OpenFilePanel("Select image", "", "", false);
+        	if (paths.Length > 0) {
+            StartCoroutine(OutputRoutine(new System.Uri(paths[0]).AbsoluteUri));
+        	}
+		}
+            
+    }
+
+	void Process()
+    {
+        ScreenCapture.CaptureScreenshot(Directory.GetCurrentDirectory() + "/" + "GeneratedMeme.png");
+    }
+
+    void Update()
+    {
+    	if(Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown("s") || Input.GetKeyDown(KeyCode.Return))
+    	{
+    		Process();
+    	}
+    }
+
+    private IEnumerator OutputRoutine(string url) {
+        var loader = new WWW(url);
+        yield return loader;
+        mainImg.GetComponent<RawImage>().texture = loader.texture;
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+
+}
