93a2923a1087b7c95fc13dd93c4898a737b005bb
Toast <98610769+ToastKamiya@users.noreply.github.com>
2022-07-11 22:14:33 +0300

    app v2.2 update
    * Added right click menu, fixed UI issues (v2.2)
    
    * app v2.2 scene files

--- importimage.cs
+++ importimage.cs
@@ -17,6 +17,7 @@ public class importimage : MonoBehaviour, IPointerClickHandler
  
     public GameObject mainImg;
     public GameObject tutorialText2;
+    public GameObject PopupMenuGO;
 
     public Text tutorialText;
 
@@ -28,6 +29,12 @@ public class importimage : MonoBehaviour, IPointerClickHandler
 
     public bool texthidden; // really don't need this but this way there are no errors being thrown that might mess with performance
 
+    public RectTransform RT;
+
+    float clicked = 0;
+    float clicktime = 0;
+    float clickdelay = 0.5f;
+
 	void Start() 
 	{
 		InvokeRepeating("Process", 0, 5f);
@@ -37,32 +44,69 @@ public class importimage : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         int clickCount = eventData.clickCount;
-		if (clickCount == 2)
+        if(clickCount == 2)
 		{
             if(!texthidden)
             {
                 tutorialText2.SetActive(false);
                 texthidden = true;
             }
-			var paths = StandaloneFileBrowser.OpenFilePanel("Select image", "", "", false);
-        	if (paths.Length > 0) {
-            StartCoroutine(OutputRoutine(new System.Uri(paths[0]).AbsoluteUri));
-        	}
+            OpenImage();
 		}
-            
+
     }
 
-	void Process()
+	public void Process()
     {
         ScreenCapture.CaptureScreenshot(Directory.GetCurrentDirectory() + "/" + "GeneratedMeme.png");
     }
 
+    public void StartProcessIE()
+    {
+        StartCoroutine(ProcessIE()); // I know how stupid this is but this is the only way to start an IEnumerator using unity button actions
+    }
+
+    public void StartSaveToIE()
+    {
+        StartCoroutine(SaveToIE()); // I know how stupid this is but this is the only way to start an IEnumerator using unity button actions
+    }
+
+    IEnumerator ProcessIE() // cause unity buttons can't invoke IEnumerators, only voids, which you can use to invoke IEnumerators
+    {
+        yield return new WaitForSeconds(0.5f);
+        ScreenCapture.CaptureScreenshot(Directory.GetCurrentDirectory() + "/" + "GeneratedMeme.png");
+    }
+
+
     void Update()
     {
-    	if(Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown("s") || Input.GetKeyDown(KeyCode.Return))
-    	{
-    		Process();
-    	}
+        if (Input.GetMouseButtonDown(1))
+        {
+            PopupMenu();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            StartCoroutine(HidePopupMenu());
+        }
+        if(Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown("s") || Input.GetKeyDown(KeyCode.Return))
+        {
+            ScreenCapture.CaptureScreenshot(Directory.GetCurrentDirectory() + "/" + "GeneratedMeme.png");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            clicked++;
+                if (clicked == 1) clicktime = Time.time;
+     
+                if (clicked > 1 && Time.time - clicktime < clickdelay)
+                {
+                    clicked = 0;
+                    clicktime = 0;
+                    OpenImage();
+                }
+
+            else if (clicked > 2 || Time.time - clicktime > 1) clicked = 0;
+        }
     }
 
     private IEnumerator OutputRoutine(string url) {
@@ -86,10 +130,55 @@ public class importimage : MonoBehaviour, IPointerClickHandler
         TopTMP.fontSize = float.Parse(LeftIF.text);
     }
 
+    void PopupMenu()
+    {
+        PopupMenuGO.SetActive(true);
+        RT.anchoredPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    }
+
+    public void OpenImage()
+    {
+        if(!texthidden)
+        {
+            tutorialText2.SetActive(false);
+            texthidden = true;
+        }
+        var paths = StandaloneFileBrowser.OpenFilePanel("Select image", "", "", false);
+        if (paths.Length > 0) 
+        {
+            StartCoroutine(OutputRoutine(new System.Uri(paths[0]).AbsoluteUri));
+        }
+    }
+
+    public IEnumerator SaveToIE()
+    {
+        yield return new WaitForSeconds(0.5f);
+        var path = StandaloneFileBrowser.SaveFilePanel("Title", "", "GeneratedMeme.png", "");
+        if (!string.IsNullOrEmpty(path)) {
+            ScreenCapture.CaptureScreenshot(path);
+        }
+    }
+
+    public IEnumerator HidePopupMenu()
+    {
+        yield return new WaitForSeconds(0.05f);
+        PopupMenuGO.SetActive(false);
+        RT.anchoredPosition = new Vector2(9999, 9999);
+    }
+
     public void SetBottomTextFontSize()
     {
         BottomTMP.fontSize = float.Parse(RightIF.text);
     }
 
+    public void OpenGithub()
+    {
+        Application.OpenURL("https://github.com/ToastKamiya/MemeMaker");
+    }
+
+    public void OpenComplaints()
+    {
+        Application.OpenURL("https://github.com/ToastKamiya/MemeMaker/issues");
+    }
 
 }
